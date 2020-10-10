fc3d488ec0ffb28b4c0fa639184a5b914b00a23c
miguel17546mals <miguel17546mals@gmail.com>
2020-10-10 15:53:11 -0500

    Semaforo con boton

--- P1/semaphore_push_button.py
+++ P1/semaphore_push_button.py
@@ -17,6 +17,8 @@ semaphore_red = LED(13)
 semaphore_yellow = LED(19)
 semaphore_green = LED(26)
 
+current_LED_name = None
+
 def transition(current_LED, next_LED, blink_t=True):
 	if blink_t:
 		for t in range(2 * blink_iter):
@@ -29,17 +31,43 @@ def led_on(current_LED,LED_time):
 	current_LED.on()
 	sleep(LED_time)
 
-semaphore_red.off()
-semaphore_yellow.off()
-semaphore_green.off()
 
-while True:
-	led_on(semaphore_green,green_time)
-	transition(semaphore_green,semaphore_yellow)
-	led_on(semaphore_yellow,yellow_time)
-	transition(semaphore_yellow,semaphore_red, blink_t=False)
-	led_on(semaphore_red,red_time)
-	transition(semaphore_red,semaphore_green, blink_t=False)
+def semaphore():
+	semaphore_red.off()
+	semaphore_yellow.off()
+	semaphore_green.off()
+	while True:
+		event.wait()
+		while not event.is_set():
+			current_LED_name = 'green'
+			led_on(semaphore_green,green_time)
+			transition(semaphore_green,semaphore_yellow)
+			current_LED_name = 'yellow'
+			led_on(semaphore_yellow,yellow_time)
+			transition(semaphore_yellow,semaphore_red, blink_t=False)
+			current_LED_name = 'red'
+			led_on(semaphore_red,red_time)
+			transition(semaphore_red,semaphore_green, blink_t=False)
+		#Handler
+		interrup(current_LED)
 
+def interrup(current_LED):
+	if current_LED == 'green':
+		led_on(semaphore_red,red_time)
+		transition(semaphore_red,semaphore_green, blink_t=False)
+		event.clear()
+
+def button_handler():
+	while True:
+		button.wait_for_press()
+		event.set()
+
+event = Event()
+
+sem = Thread(target=semaphore)
+button = Thread(target=button_handler)
+
+sem.start()
+button.start()
 
 
