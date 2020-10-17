6bedaacfc5fe082b605df53322190f5e105c007c
miguel17546mals <miguel17546mals@gmail.com>
2020-10-16 21:58:42 -0500

    Subiendo semaforo P3

--- P3/semaphore_bt.py
+++ P3/semaphore_bt.py
@@ -1,6 +1,7 @@
-from gpiozero import LED, Button
+from gpiozero import LED
 from time import sleep
-from threading import Thread,Event
+from signal import pause
+from bluedot import BlueDot
 
 blink_time = 0.5
 blink_iter = 3
@@ -12,12 +13,6 @@ green_time = 5
 semaphore_red = LED(13)
 semaphore_yellow = LED(19)
 semaphore_green = LED(26)
-
-push_button = Button(2)
-
-event = Event()
-
-time_to_stop = 1
 
 def transition(current_LED, next_LED, blink_t=True):
 	"""
@@ -46,51 +41,21 @@ def led_on(current_LED,LED_time):
 	current_LED.on()
 	sleep(LED_time)
 
-def semaphore():
-	"""
-	Funcion que realiza todas las funciones de un semaforo sencillo.
-	"""	
-	semaphore_red.off()
-	semaphore_yellow.off()
-	semaphore_green.off()
-	while True:
-		semaphore_green.on()
-		# Con este if lo que se hace es que el Evento espera una cierta cantidad de tiempo en espera de algún cambio.
-		# Este cambio se puede activar de forma asincrona desde otro thread o en general desde otra parte.
-		# Cuando se activa se 'Atiende la interrupción' y resetea el proceso.
-		if event.wait(green_time):
-			print("Se atiende la interrupcion")
-			sleep(time_to_stop)
-			interrup()
-			continue
-		transition(semaphore_green,semaphore_yellow)
-		led_on(semaphore_yellow,yellow_time)
-		transition(semaphore_yellow,semaphore_red, blink_t=False)
-		led_on(semaphore_red,red_time)
-		transition(semaphore_red,semaphore_green, blink_t=False)
+bd = BlueDot()
 
-def interrup():
-	"""
-	Función con las instrucciones necesarias para atender la interrupción.
-	"""	
+semaphore_red.off()
+semaphore_yellow.off()
+semaphore_green.off()
+
+while True:
+	led_on(semaphore_green,green_time)
+	bd.wait_for_press()
 	transition(semaphore_green,semaphore_yellow)
 	led_on(semaphore_yellow,yellow_time)
+	bd.wait_for_press()
 	transition(semaphore_yellow,semaphore_red, blink_t=False)
 	led_on(semaphore_red,red_time)
+	bd.wait_for_press()
 	transition(semaphore_red,semaphore_green, blink_t=False)
-	event.clear()
 
-def button_handler():
-	"""
-	Función que detecta cambios en el boton configurado y activa el evento
-	"""	
-	while True:
-		push_button.wait_for_press()
-		event.set()
-		
-# Se inicializan los threads para el semaforo y el handler del boton
-sem = Thread(target=semaphore)
-button = Thread(target=button_handler)
-# Se inician los threads
-sem.start()
-button.start()
+
