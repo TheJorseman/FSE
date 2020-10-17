d2015f6401e1ef857a6403b2367254dc68c18a7f
miguel17546mals <miguel17546mals@gmail.com>
2020-10-17 04:39:30 +0100

    Se suben archivos verificados de los leds con bt

--- P3/leds_bt.py
+++ P3/leds_bt.py
@@ -5,26 +5,24 @@ red = PWMLED(13)
 yellow = PWMLED(19)
 green = PWMLED(26)
 
-led = yellow
 
-def set_brightness(pos):
+def set_brightness(pos,led):
     brightness = (pos.y + 1) / 2
     led.value = brightness
 
 def dpad(pos):
-    if pos.top:
-        set_brightness(pos)
-    elif pos.bottom:
-        set_brightness(pos)
-    elif pos.left:
-        led = red
-        led.on()
-    elif pos.right:
-        led = green
-        led.on()
-    elif pos.middle:
-        led = yellow
-        led.on()
+    red.off()
+    yellow.off()
+    green.off()
+
+    print(pos.x,",",pos.y)
+
+    if pos.x < -1 + 2/3:
+        set_brightness(pos,red)
+    elif pos.x >= -1 + 2/3 and pos.x < -1 + 4/3:
+        set_brightness(pos,yellow)
+    elif pos.x >= -1 + 4/3:
+        set_brightness(pos,green)
 
 bd = BlueDot()
 while True:   
