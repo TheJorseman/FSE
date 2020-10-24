85bfd2130e5354c44ea03e01e69f74c9d7f58138
miguel17546mals <miguel17546mals@gmail.com>
2020-10-23 19:12:22 -0500

    Se agrega funcionalidad al bot

--- P4/practica4.py
+++ P4/practica4.py
@@ -2,15 +2,23 @@
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 import telebot
-
+import json
 API_TOKEN = '1088193438:AAFffJIzdeGBWtSZhzDCeoYTlkDK2O_Naq4'
 bot = telebot.TeleBot(API_TOKEN)
 
 
 
+def get_full_name(message):
+    
+
+
 # Handle '/ledon'
 @bot.message_handler(commands=['ledon'])
 def command_ledon(message):
+    message_json = json.dumps(str(message))
+    return_message = ""
+    
+    print(message_json)
     bot.reply_to(message, "xD")
 
 # Handle '/start' and '/help'
