b307bede8ab066ee7443b765d957c0794d7ac4e7
miguel17546mals <miguel17546mals@gmail.com>
2020-10-27 22:28:25 -0600

    test 4

--- Tarea/casa_inteligente_telegram.py
+++ Tarea/casa_inteligente_telegram.py
@@ -1,21 +1,17 @@
 from CasaInteligente.services.telegram_api import Telegram
 from CasaInteligente.components import foco,persiana,alarma,tira_led
 
-bot_encendido = object()
+
+alarma_obj = alarma.Alarma(0,"alarma")
+persiana_obj = persiana.Persiana(1,"persiana")
+foco_obj = foco.Foco(2,"foco")
+tira_led_obj = tira_led.TiraLED(3,"tira_led") 
+dispositivos = [alarma_obj, persiana_obj, foco_obj, tira_led_obj]
+
+bot_encendido = Telegram("Encendido","1088193438:AAFffJIzdeGBWtSZhzDCeoYTlkDK2O_Naq4")
+bot_encendido.set_elements(dispositivos)
+bot_encendido.run()
 
 @bot_encendido.message_handler(commands=['on'])
 def command_ledon(message):
     bot_encendido.command_on(message)
-
-
-
-if __name__ == "__main__":
-    alarma_obj = alarma.Alarma(0,"alarma")
-    persiana_obj = persiana.Persiana(1,"persiana")
-    foco_obj = foco.Foco(2,"foco")
-    tira_led_obj = tira_led.TiraLED(3,"tira_led") 
-    dispositivos = [alarma_obj, persiana_obj, foco_obj, tira_led_obj]
-
-    bot_encendido = Telegram("Encendido","1088193438:AAFffJIzdeGBWtSZhzDCeoYTlkDK2O_Naq4")
-    bot_encendido.set_elements(dispositivos)
-    bot_encendido.run()
